4a5c4fff69128b1021f61a679a3d394dbf5d61da
Brian Haley <bhaley9@gatech.edu>
2022-03-07 19:10:33 -0600

    deleted unused files

--- src/main/java/com/akshayapatravms/c4g/enums/PresenceModality.java
+++
@@ -1,8 +0,0 @@
-package com.akshayapatravms.c4g.enums;
-
-public enum PresenceModality {
-    //    since we are using @Enumerated(EnumType.STRING), changing these values will break the data, you can add new values, but don't
-    //    change the present ones (ie IN_PERSON -> PHYSICAL)
-    IN_PERSON,
-    VIRTUAL,
-}
