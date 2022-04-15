5a341818bfedd7037b1545b73387f924af1e73bf
dylanrasch <dylan.rasch@telenetgroup.be>
2022-04-15 18:30:20 +0200

    images load faster

--- src/main/webapp/app/akshaya-patra-app/services/image/image.service.ts
+++ src/main/webapp/app/akshaya-patra-app/services/image/image.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { ApplicationConfigService } from "../application-config/application-config.service";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +17,20 @@ export class ImageService {
               private sanitizer: DomSanitizer) {
   }
 
+  imageCache = {};
+
   imageById$(id: string): Observable<SafeUrl> {
-    return this.http.get(this.applicationConfigService.getEndpointFor(`${this.API_PREFIX}/${id}`), {responseType: 'blob'}).pipe(
-      //TODO: do more research to make sure this is 100% safe
-      map(val => this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(val)))
-    )
+    if (this.imageCache[id]) {
+      return of(this.imageCache[id]);
+    } else {
+      return this.http.get(this.applicationConfigService.getEndpointFor(`${this.API_PREFIX}/${id}`), {responseType: 'blob'}).pipe(
+        //TODO: do more research to make sure this is 100% safe
+        map(val => this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(val))),
+        tap((safeUrl) => {
+          this.imageCache[id] = safeUrl;
+        })
+      );
+    }
   }
 
 }
