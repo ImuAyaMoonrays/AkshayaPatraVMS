400c1437461d0afdb20003e1725633c8dc461fb1
dylanrasch <dylan.rasch@telenetgroup.be>
2022-04-20 12:34:07 +0200

    bug fixes frontend

---
+++ src/main/webapp/app/akshaya-patra-app/pipes/causes/causes.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CausesPipe } from './causes.pipe';
+
+describe('CausesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CausesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
